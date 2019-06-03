6fbb98b2323ef507a42bdac2fd96e12c9f9a1246
Vishnu <vishnu.n@qentelli.com>
2019-06-03 14:56:59 +0530

    updated oats location

--- src/main/java/com/beachbody/automation/drivers/OATSDriver.java
+++ src/main/java/com/beachbody/automation/drivers/OATSDriver.java
@@ -1,7 +1,9 @@
 package com.beachbody.automation.drivers;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -27,7 +29,14 @@ public class OATSDriver {
 		ProcessBuilder oatsProcess = new ProcessBuilder("C:/OracleATS/openScript/runScript.bat",scriptName+"/"+scriptName+".jwg");
 		oatsProcess.directory(new File("OATS/Scripts"));
 		try {
+			oatsProcess.redirectErrorStream(true);
 			Process p = oatsProcess.start();
+			BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			String line;
+			while ((line = in.readLine()) != null) {
+			    System.out.println(line);
+			}
+			
 			p.waitFor();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
